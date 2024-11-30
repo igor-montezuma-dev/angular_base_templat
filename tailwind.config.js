7164a01b4d0df654f5789aa0c392f2588a6489ba
IgorMontezuma20 <igorminranda17@gmail.com>
2024-11-30 12:22:02 -0300

    config: Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#1D2429',
+        red: '#DC282D',
+        green: '#54920E',
+        'gray-100': '#D9D9D9',
+        blue: '#2989DA',
+        'black-100': '#282531'
+
+      },
+    },
+  },
+  plugins: [],
+}
+
